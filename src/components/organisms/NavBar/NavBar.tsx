750b0556f998e9caa8d21143260eb2ec6f119a06
Даниил Ицыксон <daniilitsyxon@gmail.com>
2025-10-05 18:03:48 +0300

    fix: исправил logout

--- src/components/organisms/NavBar/NavBar.tsx
+++ src/components/organisms/NavBar/NavBar.tsx
@@ -8,7 +8,7 @@ const NavBar = () => {
     const router = useRouter()
     const handleLogout = () => {
         document.cookie =
-            'authToken=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT'
+            'auth-token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT'
         router.push('/login')
 
     }
